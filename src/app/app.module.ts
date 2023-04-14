2a2fb0edc55ff29024b2c1b92fdcf7c57862c096
Java FS <javafs4u@gmail.com>
2023-04-14 18:26:04 +0530

    fix compile issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { Comp1Component } from './comp1/comp1.component';
-import { Comp2Component } from './comp2/comp2.component';
 import { CounterComponent } from './counter/counter.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
@@ -12,8 +10,6 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component,
-    Comp2Component,
     CounterComponent,
     TodoComponent,
     TodoItemComponent
